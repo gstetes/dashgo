2ab1727bd4a63824d0e0bfb0f1ca5bb782be5c45
Guilherme Stetes <guilherme@dobesone.com.br>
2021-12-27 16:11:23 -0300

    271221

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,19 @@
-import { Box, Button, Flex, Heading, Icon } from '@chakra-ui/react';
-import { RiAddLine } from 'react-icons/ri';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Text,
+} from '@chakra-ui/react';
+import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 
@@ -22,11 +36,53 @@ export default function UserList() {
               size="sm"
               fontSize="small"
               colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} />}
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
             >
               Criar novo
             </Button>
           </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Guilherme Stetes</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      gstetes@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>04 de Abril, 2021</Text>
+                </Td>
+                <Td>
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
         </Box>
       </Flex>
     </Box>
