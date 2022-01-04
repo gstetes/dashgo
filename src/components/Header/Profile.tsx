86600cf848fb54c57833bb98086aef6be94ea09c
Guilherme Stetes <guilherme@dobesone.com.br>
2022-01-04 13:53:18 -0300

    04012022

---
+++ src/components/Header/Profile.tsx
@@ -0,0 +1,26 @@
+import { Box, Flex, Text, Avatar } from '@chakra-ui/react';
+
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
+  return (
+    <Flex align="center">
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Guilherme Stetes</Text>
+          <Text color="gray.300" fontSize="small">
+            gstetes@gmail.com
+          </Text>
+        </Box>
+      )}
+
+      <Avatar
+        size="md"
+        name="Guilherme Stetes"
+        src="https://github.com/gstetes.png"
+      />
+    </Flex>
+  );
+}
